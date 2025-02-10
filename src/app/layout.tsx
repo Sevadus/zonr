9fbee3b3ec6e6f8a47f47b25c3e9fd37c59e0af0
Matthew Zagursky <Sevadus@users.noreply.github.com>
2025-02-10 16:33:43 -0600

    this is for cloudflare

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { ThemeProvider } from 'next-themes';
+
 import './globals.css';
+
+export const runtime = 'edge';
 
 export const metadata: Metadata = {
   title: 'zonr - Share your Time',
