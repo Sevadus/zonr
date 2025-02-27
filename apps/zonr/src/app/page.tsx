94493afc935c8fa43677ff492e5c49d7356b325b
Matt Zagursky <matt@mattzagursky.com>
2025-02-27 12:13:27 -0600

    Quick validation add.

--- apps/zonr/src/app/page.tsx
+++ apps/zonr/src/app/page.tsx
@@ -16,9 +16,10 @@ export async function generateMetadata({
 
   const baseUrl = new URL(process.env.NEXT_PUBLIC_BASE_URL || 'https://zonr.dev')
 
-  const description = dt
-    ? `Time shared: ${DateTime.fromISO(dt, { setZone: true }).toFormat('FFFF')}. Click for instant conversion to your local timezone.`
-    : 'The easiest way to share your time with others in different timezones.'
+  const description =
+    dt && isValidDateTime(dt)
+      ? `Time shared: ${DateTime.fromISO(dt, { setZone: true }).toFormat('FFFF')}. Click for instant conversion to your local timezone.`
+      : 'The easiest way to share your time with others in different timezones.'
 
   const metadata: Metadata = {
     title: 'zonr - Stay in Sync with a Single Link',
@@ -38,6 +39,13 @@ export async function generateMetadata({
   return metadata
 }
 
+function isValidDateTime(dt: string) {
+  if (dt.length < 4 || dt.length > 25 || dt === 'undefined') {
+    return false
+  }
+  return true
+}
+
 export default async function Home({
   searchParams,
 }: {
@@ -46,13 +54,14 @@ export default async function Home({
 }) {
   const dt = decodeURIComponent((await searchParams).dt as string)
   console.log(dt)
+  console.log(isValidDateTime(dt))
   return (
     <div className="bg-background flex min-h-screen flex-col items-center justify-center p-4">
       <div className="fixed right-4 top-4">
         <ThemeToggle />
       </div>
       <main className="mx-auto w-full max-w-3xl space-y-6">
-        {dt && <DtConvertCard dt={dt} />}
+        {dt && isValidDateTime(dt) && <DtConvertCard dt={dt} />}
         <DtConfigCard />
         <div className="text-muted-foreground text-center text-sm">
           <p>Stay in sync with a single link.</p>
