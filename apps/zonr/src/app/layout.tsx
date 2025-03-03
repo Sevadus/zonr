9b5e22b87209711133588ee04cef1e91cd2c72ce
Matt Zagursky <matt@mattzagursky.com>
2025-03-03 12:41:07 -0600

    added metadataBase to the layout file to maybe help?

--- apps/zonr/src/app/layout.tsx
+++ apps/zonr/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import { Toaster } from '@/components/ui/sonner'
+import { Metadata } from 'next'
 import { ThemeProvider } from 'next-themes'
 
 import './globals.css'
 
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL || 'https://zonr.dev'),
+}
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
